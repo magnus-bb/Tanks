077e8855c1fc5916ac2ec043f891f39a844471cd
CrispyNipsy <magnus.borregaard@gmail.com>
2019-11-26 12:41:26 +0100

    .

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ config = {
 	fps: 60,
 	player: {
 		diameter: 20,
-		moveSpeed: 2,
+		moveSpeed: 2, // Has to be less than the width of walls to not pass through
 		turnSpeed: 4.5,
-		color: [30, 170, 30],
 		cannonLength: 18,
 		ammo: 3
 	},
@@ -16,7 +15,7 @@ config = {
 		wallChance: 0.5//*0.2
 	},
 	bullet: {
-		speed: 3,
+		speed: 3, // Has to be less than the width of walls to not pass through
 		diameter: 8,
 		framesAlive: 999 //*60 * 6 // 60fps * 5seconds
 	}
